d7c77b8cf2271b18f7a2cf9d339839a8e46e0195
kristina15 <krisy-15012001@yandex.ru>
2021-09-19 16:39:17 +0400

    add project

---
+++ GraphTheory/GraphTheory/Validators/EdgeValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using GraphTheory.Entites.HelperEntites;
+
+namespace GraphTheory.Validators
+{
+    public class EdgeValidator:AbstractValidator<Edge>
+    {
+        public EdgeValidator()
+        {
+            RuleFor(e => e.From).NotNull().WithMessage("Uncorrect 'from' vertex");
+            RuleFor(e => e.To).NotNull().WithMessage("Uncorrect 'to' vertex");
+        }
+
+        public override ValidationResult Validate(ValidationContext<Edge> context)
+        {
+            return base.Validate(context);
+        }
+    }
+}
